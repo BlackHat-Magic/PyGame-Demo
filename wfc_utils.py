afd1dadfacd20f4700246e903fad7917c9f3cb0b
Luke Henderson <personal@lukeh.us>
2023-11-18 11:58:54 -0800

    some required neighbors stuff

--- wfc_utils.py
+++ wfc_utils.py
@@ -123,6 +123,76 @@ def propagate(grid, start_cell):
             # propagate
             if(updated and (not (adj_x, adj_y) in queue)):
                 queue.append((adj_x, adj_y))
+        
+        # if the cell we're propagating hasn't collapsed or doesn't have a required neighbor or meets
+        # its requirements, we're done with it
+        if(grid[x, y].requirementsMet()):
+            continue
+        
+        # grab target tile
+        target_tile = grid[x, y]
+        required_neighbors = target_tile.biome.required_neighbors
+
+        # get neighbors and sort by entropy
+        neighbors = [
+            (x + x_offset, y + y_offset) for x_offset, y_offset in [
+                (-1, 1), (0, 1), (1, 1), (-1, 0), (1, 0), (-1, -1), (0, -1), (1, -1)
+            ]
+        ]
+        neighbors = sorted(neighbors, key=lambda coords: grid[coords].entropy() for coords in neighbors)
+
+        # loop through neighbors and collapse as required
+        for coords in neighbors:
+            neighbor = grid[coords]
+            for requirement in target_cell.required_neighbors:
+                if(not requirement in grid[coords].possibilities):
+                    continue
+                result = neighbor.collapse(requirement)
+                if(not result):
+                    input("We goofed up.")
+                    return
+                if(coords not in queue):
+                    queue.append(coords)
+
+def requirementsMet(x, y):
+    # if the cell hasn't collapsed yet and not all of the possibilities have requirements, the
+    # requirements of the cell are met by default, since it's possible that the cell won't have a
+    # requirement at all
+    if((not grid[x, y].biome) and (not all([possibility.requirements for possibility in grid[x, y].possibilities]))):
+        return(True)
+    # if the cell has collapsed but has no requirements, the requiremetns are met by default
+    if(grid[x, y].biome and not grid[x, y].biome.required_neighbors):
+        return(True)
+
+    # essentially, get a collection all allowed neighbors
+    # if the cell is collapsed, `possibilities` will have length 1
+    # allowed states are based on requirements, not literal allowed_neighbors
+    allowed_states = set()
+    for possibility in grid[x, y].possibilities:
+        for requirement in possibility.required_neighbors:
+            allowed_states.add(requirement)
+    allowed_states = [state for state in allowed_states]
+
+    # get all the neighbors
+    offsets = [(-1, 1), (0, 1), (1, 1), (-1, 0), (1, 0), (-1, -1), (0, -1), (1, -1)]
+    neighbors = []
+    for x_offset, y_offset in offsets:
+        adj_x = x + x_offset
+        adj_y = y + y_offset
+        if((not 0 <= adj_x < grid.shape[0]) or (not 0 <= adj_y < grid.shape[1])):
+            continue
+        neighbor = grid[adj_x, adj_y]
+        if(not neighbor.biome):
+            continue
+        neighbors.append(neighbor.biome)
+    
+    # loop through all neighbors, if possibilities for a given tile are not all contained in
+    # allowed_states, return false
+    for neighbor in neighbors:
+        possibilities = set(neighbor.possibilities)
+        if(intersection(possibilities, allowed_states) != possibilities):
+            return(False)
+    return(True)
 
 # when a cell is propagated, each neighboring cell 
 def updateStates(grid, x, y, adj_x, adj_y):

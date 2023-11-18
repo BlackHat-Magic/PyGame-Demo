915cfa8f0c1c27beef4fccf4eb98206381f5354d
Luke Henderson <personal@lukeh.us>
2023-11-18 15:02:08 -0800

    refactored so that world is a class

--- wfc_utils.py
+++ wfc_utils.py
@@ -10,15 +10,23 @@ class Biome():
 
 # tile class
 class Tile():
-    def __init__(self, biome: Biome = None, possibilities: list = []):
+    def __init__(self, coordinates: tuple, world: object, biome: Biome = None, possibilities: list = []):
+        self.coordinates = coordinates
+        self.world = world
         self.biome = biome
         if(self.biome):
             self.possibilities = [biome]
         else:
             self.possibilities = possibilities
+    
+    # cell entropy
     def entropy(self):
         # easier if this is a method instead of an attribute
+        if(self.biome):
+            return(0)
         return(len(self.possibilities))
+
+    # collapse the cell
     def collapse(self, collapse_to: Biome = None):
         # if no possibilities are left, we can't collapse
         if(not self.possibilities):
@@ -33,6 +41,176 @@ class Tile():
         self.biome = collapse_to
         self.possibilities = [self.biome]
         return(self.biome)
+
+    # get cell neighbors
+    def getNeighbors(self):
+        # allows diagonals
+        offsets = [(-1, 1), (0, 1), (1, 1), (-1, 0), (1, 0), (-1, -1), (0, -1), (1, -1)]
+        neighbors = []
+
+        # loop through possible offsets
+        for x_offset, y_offset in offsets:
+            adj_x, adj_y = self.coordinates[0] + x_offset, self.coordinates[1] + y_offset
+            # if attempting to check a cell out of bounds, skip
+            if((not 0 <= adj_x < self.world.grid.shape[0]) or (not 0 <= adj_y < self.world.grid.shape[1])):
+                continue
+            neighbors.append(self.world.grid[adj_x, adj_y])
+        return(neighbors)
+    
+    # update state upon propagation
+    def updateState(self, origin_cell):
+        # store original states
+        origin_cell = self.world.grid[origin_cell]
+
+        original_possibilities = self.possibilities
+
+        # if this cell has already collapsed, we can skip it.
+        if(self.biome):
+            return(False)
+
+        new_possibilities = set()
+        # if the origin cell collapsed...
+        if(origin_cell.biome):
+            # remove all possibilities that are not allqwowed by the cell who 
+            # triggered the function
+            new_possibilities = set(self.possibilities)
+            new_possibilities.intersection_update(origin_cell.biome.allowed_neighbors)
+        
+        # if the origin cell hasn't collapsed...
+        else:
+            # assemble a set of possibilities allowed by the origin cell
+            for possibility in origin_cell.possibilities:
+                if(not new_possibilities):
+                    new_possibilities = set(possibility.allowed_neighbors)
+                    continue
+                for neighbor in possibility.allowed_neighbors:
+                    new_possibilities.add(neighbor)
+
+        # intersect new possibilities with old ones
+        new_possibilities = set(self.possibilities).intersection(new_possibilities)
+        self.possibilities = list(new_possibilities)
+        
+        return(self.possibilities != original_possibilities)
+    
+    def requirementsMet():
+        # if the cell hasn't collapsed yet and not all of the possibilities have requirements, the
+        # requirements of the cell are met by default, since it's possible that the cell won't have a
+        # requirement at all
+        if((not self.biome) and (not all([possibility.required_neighbors for possibility in self.possibilities]))):
+            return(True)
+        # if the cell has collapsed but has no requirements, the requiremetns are met by default
+        if(self.biome and not self.biome.required_neighbors):
+            return(True)
+
+        # essentially, get a collection all allowed neighbors
+        # allowed states are based on requirements, not literal allowed_neighbors
+        allowed_states = set()
+        for possibility in self.possibilities:
+            for requirement in possibility.required_neighbors:
+                allowed_states.add(requirement)
+
+        # get all the neighbors
+        neighbors = []
+        for neighbor in self.getNeighbors():
+            if(not neighbor.biome):
+                continue
+            neighbors.append(neighbor.biome)
+        
+        # loop through all neighbors, if possibilities for a given tile are not all contained in
+        # allowed_states, return false
+        for neighbor in neighbors:
+            possibilities = neighbor.possibilities
+            if(not all([possibility in allowed_states for possibility in possibilities])):
+                return(False)
+        return(True)
+
+class World():
+    # init function
+    def __init__(self, name: str = "world", biomes: list = [], size: tuple = (1, 1), pickle = None):
+        self.name = name
+        self.biomes = biomes
+        self.grid = numpy.empty(size, dtype=object)
+        for x in range(size[0]):
+            for y in range(size[1]):
+                grid[x, y] = Tile(coordinates=(x, y), world=self)
+        # miscellaneous data
+        self.pickle = pickle
+
+    # observe the grid
+    def observe(self):
+        min_entropy = len(self.biomes)
+        lowest_tiles = []
+
+        # loop through cells
+        for x, row in enumerate(self.grid):
+            for y, cell in enumerate(row):
+                entropy = cell.entropy()
+
+                # if new lowest entropy, set lowest entropy
+                if(entropy < min_entropy):
+                    min_entropy = entropy
+                    lowest_tiles = [(x, y)]
+                    continue
+
+                # else if == lowest entropy, add to lowest tiles
+                if(entropy == min_entropy):
+                    lowest_tiles.append((x, y))
+        
+        # if no tiles can be collapsed, return
+        if(not lowest_tiles):
+            return
+        
+        # else, randomly select a cell to collapse
+        x, y = random.choice(lowest_tiles)
+        self.grid[x, y].collapse()
+        return(x, y)
+    
+    # propagate changes
+    def propagate(self, start_cell):
+        # queue of cells whose states have changed and therefore need their neighbors'
+        # states changed
+        queue = deque([start_cell])
+        
+        # while the queue isn't empty
+        while(queue):
+            x, y = queue.popleft()
+            target_tile = self.grid[x, y]
+            neighbors = target_tile.getNeighbors()
+
+            # loop through adjacent cells (including diagonals)
+            for neighbor in neighbors:
+                # update possible states
+                updated = neighbor.updateState((x, y))
+
+                # if we updated, then the updated cell gets added to the queue of cells that need to
+                # propagate
+                if(updated and (not neighbor.coordinates in queue)):
+                    queue.append(neighbor.coordinates)
+            
+            # if the cell we're propagating hasn't collapsed or doesn't have a required neighbor or meets
+            # its requirements, we're done with it
+            if(target_tile.requirementsMet()):
+                continue
+            
+            # grab target tile
+            required_neighbors = target_tile.biome.required_neighbors
+
+            # sort neighbors by entropy
+            neighbors = sorted(neighbors, key=lambda neighbor: neighbor.entropy())
+
+            # loop through neighbors and collapse as required
+            for tile in neighbors:
+                corods = neighbor.coordinates
+                neighbor = self.grid[coords]
+                for requirement in target_cell.required_neighbors:
+                    if(not requirement in self.grid[coords].possibilities):
+                        continue
+                    result = neighbor.collapse(requirement)
+                    if(not result):
+                        input("We goofed up.")
+                        return
+                    if(coords not in queue):
+                        queue.append(coords)
 
 # define biomes
 forest = Biome("Forest")
@@ -63,169 +241,3 @@ volcano.allowed_neighbors = [mountains]
 
 # list of biomes
 biomes = [forest, mountains, plains, desert, swamp, coastal, ocean, taiga, ice, death_ice]
-
-grid_size = (25, 25)
-grid = numpy.empty(grid_size, dtype=object)
-for x in range(grid_size[0]):
-    for y in range(grid_size[1]):
-        grid[x, y] = Tile()
-
-def observe(grid):
-    min_entropy = len(biomes)
-    lowest_tiles = []
-
-    # loop through cells
-    for x, row in enumerate(grid):
-        for y, cell in enumerate(row):
-            entropy = item.entropy()
-            # if new lowest entropy, set lowest entropy
-            if(entropy < min_entropy):
-                min_entropy = entropy
-                lowest_tiles = [(x, y)]
-                continue
-            # else if == lowest entropy, add to lowest tiles
-            if(entropy == min_entropy):
-                lowest_tiles.append((x, y))
-    
-    # if no tiles can be collapsed, return
-    if(not lowest_tiles):
-        return
-    
-    # else, randomly select a cell to collapse
-    chosen_tile = random.choice(lowest_tiles)
-    grid[chosen_tile].collapse()
-    return(chosen_tile)
-
-# propagate changed tiles
-def propagate(grid, start_cell):
-    # queue of cells whose states have changed and therefore need their neighbors'
-    # states changed
-    queue = deque([start_cell])
-    
-    # while the queue isn't empty
-    while(queue):
-        x, y = queue.popleft()
-
-        # loop through adjacent cells (including diagonals)
-        for x_offset, y_offset in [
-            (-1, 1), (0, 1), (1, 1), (-1, 0), (1, 0), (-1, -1), (0, -1), (1, -1)
-        ]:
-            adj_x, adj_y = x + x_offset, y + y_offset
-
-            # if attempting to check a cell out of bounds, skip
-            if((not 0 <= adj_x < grid.shape[0]) or (not 0 <= adj_y < grid.shape[1])):
-                continue
-
-            # else update possible states
-            updated = updateStates((x, y), (adj_x, adj_y))
-
-            # if we updated, then the updated cell gets added to the queue of cells that need to
-            # propagate
-            if(updated and (not (adj_x, adj_y) in queue)):
-                queue.append((adj_x, adj_y))
-        
-        # if the cell we're propagating hasn't collapsed or doesn't have a required neighbor or meets
-        # its requirements, we're done with it
-        if(grid[x, y].requirementsMet()):
-            continue
-        
-        # grab target tile
-        target_tile = grid[x, y]
-        required_neighbors = target_tile.biome.required_neighbors
-
-        # get neighbors and sort by entropy
-        neighbors = [
-            (x + x_offset, y + y_offset) for x_offset, y_offset in [
-                (-1, 1), (0, 1), (1, 1), (-1, 0), (1, 0), (-1, -1), (0, -1), (1, -1)
-            ]
-        ]
-        neighbors = sorted(neighbors, key=lambda coords: grid[coords].entropy() for coords in neighbors)
-
-        # loop through neighbors and collapse as required
-        for coords in neighbors:
-            neighbor = grid[coords]
-            for requirement in target_cell.required_neighbors:
-                if(not requirement in grid[coords].possibilities):
-                    continue
-                result = neighbor.collapse(requirement)
-                if(not result):
-                    input("We goofed up.")
-                    return
-                if(coords not in queue):
-                    queue.append(coords)
-
-def requirementsMet(x, y):
-    # if the cell hasn't collapsed yet and not all of the possibilities have requirements, the
-    # requirements of the cell are met by default, since it's possible that the cell won't have a
-    # requirement at all
-    if((not grid[x, y].biome) and (not all([possibility.requirements for possibility in grid[x, y].possibilities]))):
-        return(True)
-    # if the cell has collapsed but has no requirements, the requiremetns are met by default
-    if(grid[x, y].biome and not grid[x, y].biome.required_neighbors):
-        return(True)
-
-    # essentially, get a collection all allowed neighbors
-    # if the cell is collapsed, `possibilities` will have length 1
-    # allowed states are based on requirements, not literal allowed_neighbors
-    allowed_states = set()
-    for possibility in grid[x, y].possibilities:
-        for requirement in possibility.required_neighbors:
-            allowed_states.add(requirement)
-    allowed_states = [state for state in allowed_states]
-
-    # get all the neighbors
-    offsets = [(-1, 1), (0, 1), (1, 1), (-1, 0), (1, 0), (-1, -1), (0, -1), (1, -1)]
-    neighbors = []
-    for x_offset, y_offset in offsets:
-        adj_x = x + x_offset
-        adj_y = y + y_offset
-        if((not 0 <= adj_x < grid.shape[0]) or (not 0 <= adj_y < grid.shape[1])):
-            continue
-        neighbor = grid[adj_x, adj_y]
-        if(not neighbor.biome):
-            continue
-        neighbors.append(neighbor.biome)
-    
-    # loop through all neighbors, if possibilities for a given tile are not all contained in
-    # allowed_states, return false
-    for neighbor in neighbors:
-        possibilities = set(neighbor.possibilities)
-        if(intersection(possibilities, allowed_states) != possibilities):
-            return(False)
-    return(True)
-
-# when a cell is propagated, each neighboring cell 
-def updateStates(grid, x, y, adj_x, adj_y):
-    # store original states
-    target_cell = grid[adj_x, adj_y]
-    origin_cell = grid[x, y]
-
-    original_possibilities = target_cell.possibilities
-
-    # if this cell has already collapsed, we can skip it.
-    if(target_cell.biome):
-        return False
-
-    new_possibilities = set()
-    # if the origin cell collapsed...
-    if(origin_cell.biome):
-        # remove all possibilities that are not allqwowed by the cell who 
-        # triggered the function
-        new_possibilities = set(target_cell.possibilities)
-        new_possibilities.intersection_update(origin_cell.biome.allowed_neighbors)
-    
-    # if the origin cell hasn't collapsed...
-    else:
-        # assemble a set of possibilities allowed by the origin cell
-        for possibility in origin_cell.possibilities:
-            if(not new_possibilities):
-                new_possibilities = set(possibility.allowed_neighbors)
-                continue
-            for neighbor in possibility.allowed_neighbors:
-                new_possibilities.add(neighbor)
-
-    # intersect new possibilities with old ones
-    new_possibilities = set(target_cell.possibilities).intersection(new_possibilities)
-    target_cell.possibilities = list(new_possibilities)
-    
-    return(target_cell.possibilities != original_possibilities)
